4dbf6bbaa451696ec5afa810f062b1579c950c15
supriya-1234 <66170913+supriya-1234@users.noreply.github.com>
2020-12-05 19:01:29 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -28,52 +28,7 @@ app.get("/newFeeds", (req, res) => {
     }
   });
 });
-app.get("/newFeeds/:limit", (req, res) => {
-  let par = req.params;
-  let limit = 10;
-  let offset = 0;
-  if (!isNaN(Number(par.limit))) {
-    limit = par.limit;
-  }
-  newsArticleModel.find({}, function (err, result) {
-    if (err) {
-      res.send(err);
-    } else {
-      let newRes = [];
-      for (let i = 0; i < limit; i++) {
-        newRes.push(result[i]);
-      }
-      console.log(newRes.length);
-      res.status(200).send(newRes);
-    }
-  });
-});
-app.get("/newFeeds/:limit/:offset", (req, res) => {
-  let par = req.params;
-  let limit = 10;
-  let offset = 0;
-  if (!isNaN(Number(par.limit))) {
-    limit = par.limit;
-  }
-  if (!isNaN(Number(par.offset))) {
-    offset = par.offset;
-  }
-  newsArticleModel.find({}, function (err, result) {
-    if (err) {
-      res.send(err);
-    } else {
-      let newRes = [];
-      for (let i = 0; i < limit; i++) {
-        newRes.push(result[i]);
-      }
-      for (let i = 0; i < offset; i++) {
-        newRes.pop();
-      }
-      console.log(newRes.length);
-      res.status(200).send(newRes);
-    }
-  });
-});
+
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
